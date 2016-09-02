1f7b6edc0a09f24540043d06f85f1573ad51d3e2
Nigel Horne <njh@bandsman.co.uk>
2016-09-02 12:06:50 -0400

    Link to the TODO page from the Calendar

--- dynamic-site/lib/Gedsite/Display/calendar.pm
+++ dynamic-site/lib/Gedsite/Display/calendar.pm
@@ -18,18 +18,12 @@ sub html {
 	my $info = $self->{_info};
 	my $allowed = {
 		'page' => 'calendar',
-		'month' => qr(^\d{1,2}$),	# must be one or two digits
+		# 'month' => qr(^\d{1,2}$),	# must be one or two digits
 	};
 	my $params = $info->params({ allow => $allowed });
 	if($params && $params->{'page'}) {
 		delete $params->{'page'};
 	}
-
-	# unless($params && scalar(keys %{$params})) {
-		# # Display the main calendar page
-		# # TODO: use the current month as the default
-		# return $self->SUPER::html({ months => DateTime::Locale->load($self->{_lingua}->language())->month_format_wide() });
-	# }
 
 	if(my $month = $params->{'month'}) {
 		# Handles into the databases
@@ -41,7 +35,6 @@ sub html {
 			# TODO: sort by name
 			push @{$events[$event->{'day'} - 1]}, $event;
 		}
-		my $dt = DateTime->today();
 		return $self->SUPER::html({
 			events => \@events,
 			month => @{DateTime::Locale->load($self->{_lingua}->language())->month_format_wide()}[$month - 1],
@@ -49,7 +42,10 @@ sub html {
 		});
 	} else {
 		my $history = $args{'history'};
-		return $self->SUPER::html({ events => $history->selectall_hashref() });
+		return $self->SUPER::html({
+			events => $history->selectall_hashref(),
+			updated => $history->updated()
+		});
 	}
 
 	return $self->SUPER::html();
