469506659672bc36e68ce78d2f7c0cda07df8aee
Nigel Horne <njh@bandsman.co.uk>
2016-12-27 11:36:06 -0500

    Allow the surnames page to support the lang parameter

--- dynamic-site/lib/Ged2site/Display/surnames.pm
+++ dynamic-site/lib/Ged2site/Display/surnames.pm
@@ -10,21 +10,21 @@ sub html {
 	my $self = shift;
 	my %args = (ref($_[0]) eq 'HASH') ? %{$_[0]} : @_;
 
-	my $info = $self->{_info};
 	my $allowed = {
 		'page' => 'surnames',
 		'surname' => qr/[A-Z\s]+/i,
+		'lang' => qr/^[A-Z][A-Z]/i,
 	};
-	my $params = $info->params({ allow => $allowed });
-	if($params && $params->{'page'}) {
-		delete $params->{'page'};
-	}
+	my %params = %{$self->{_info}->params({ allow => $allowed })};
+
+	delete $params{'page'};
+	delete $params{'lang'};
 
 	# Handles into the databases
 	my $surnames = $args{'surnames'};
 	my $people = $args{'people'};
 
-	unless($params && scalar(keys %{$params})) {
+	unless(scalar(keys %params)) {
 		# Display the main index page
 		my @s = $surnames->surname();
 		return $self->SUPER::html({ surnames => \@s, updated => $surnames->updated() });
@@ -32,7 +32,7 @@ sub html {
 
 	# Look in the surnames.csv for the name given as the CGI argument and
 	# find their details
-	my $surname = $surnames->selectall_hashref($params);
+	my $surname = $surnames->selectall_hashref(\%params);
 	my @people = map { $people->fetchrow_hashref({ entry => $_->{'person'} }) } @{$surname};
 
 	# TODO: handle situation where look up fails
