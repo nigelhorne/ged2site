d23e620d00a7543b103ae51e10794cc8b19c3f9a
Nigel Horne <njh@bandsman.co.uk>
2017-01-02 12:19:24 -0500

    Fill in the current year

--- dynamic-site/lib/Ged2site/Display/calendar.pm
+++ dynamic-site/lib/Ged2site/Display/calendar.pm
@@ -19,28 +19,30 @@ sub html {
 	my $allowed = {
 		'page' => 'calendar',
 		# 'month' => qr(^\d{1,2}$),	# must be one or two digits
+		'lang' => qr/^[A-Z][A-Z]/i,
 	};
 	my $params = $info->params({ allow => $allowed });
 
 	# Handle into the database
 	my $history = $args{'history'};
 
-	if(my $month = $params->{'month'}) {
-		my @events;
+	# if(my $month = $params->{'month'}) {
+		# my @events;
 
-		foreach my $event(@{$history->selectall_hashref({ month => $month })}) {
-			# TODO: sort by name
-			push @{$events[$event->{'day'} - 1]}, $event;
-		}
-		return $self->SUPER::html({
-			events => \@events,
-			month => @{DateTime::Locale->load($self->{_lingua}->language())->month_format_wide()}[$month - 1],
-			year => DateTime->today()->year()
-		});
-	}
+		# foreach my $event(@{$history->selectall_hashref({ month => $month })}) {
+			# # TODO: sort by name
+			# push @{$events[$event->{'day'} - 1]}, $event;
+		# }
+		# return $self->SUPER::html({
+			# events => \@events,
+			# month => @{DateTime::Locale->load($self->{_lingua}->language())->month_format_wide()}[$month - 1],
+			# year => DateTime->today()->year()
+		# });
+	# }
 	return $self->SUPER::html({
 		events => $history->selectall_hashref(),
-		updated => $history->updated()
+		updated => $history->updated(),
+		year => DateTime->today()->year()
 	});
 }
 
