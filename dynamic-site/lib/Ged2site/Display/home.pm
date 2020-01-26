272e0357bc500115e2a6c652b19bbddaa1b5604f
Nigel Horne <njh@bandsman.co.uk>
2020-01-26 15:17:57 -0500

    Change the first page from the home person to a list of today's historic events

---
+++ dynamic-site/lib/Ged2site/Display/home.pm
@@ -0,0 +1,44 @@
+package Ged2site::Display::home;
+
+use warnings;
+use strict;
+
+# Display the home page - list today's events
+# TODO: More than just BMD, for example baptisms and travelling
+
+use Ged2site::Display;
+
+our @ISA = ('Ged2site::Display');
+
+sub html {
+	my $self = shift;
+	my %args = (ref($_[0]) eq 'HASH') ? %{$_[0]} : @_;
+
+	my $info = $self->{_info};
+	die unless($info);
+
+	my $allowed = {
+		'page' => 'home',
+		'lang' => qr/^[A-Z][A-Z]/i,
+		'lint_content' => qr/^\d$/,
+	};
+	my %params = %{$info->params({ allow => $allowed })};
+
+	delete $params{'page'};
+	delete $params{'lint_content'};
+	delete $params{'lang'};
+
+	my $history = $args{'history'};
+	my $today = DateTime->today();
+	my $events = $history->selectall_hashref({
+		day => $today->day(),
+		month => $today->month()
+	});
+
+	return $self->SUPER::html(
+		events => $events,
+		updated => $history->updated()
+	);
+}
+
+1;
