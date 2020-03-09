9f10f422c9010e6fa5ef069b62d38bb956ab4544
Nigel Horne <njh@bandsman.co.uk>
2020-03-09 10:43:11 -0400

    Fix case when there are no military records

--- dynamic-site/lib/Ged2site/Display/military.pm
+++ dynamic-site/lib/Ged2site/Display/military.pm
@@ -12,18 +12,22 @@ sub html {
 	my $self = shift;
 	my %args = (ref($_[0]) eq 'HASH') ? %{$_[0]} : @_;
 
-	my @military = $args{'military'}->selectall_hash();
+	if(my $military = $args{'military'}) {
+		my @military = $military->selectall_hash();
 
-	my $people = $args{'people'} || die;
+		my $people = $args{'people'} || die;
 
-	# Lookup the full name of the person
-	foreach my $person(@military) {
-		$person->{'title'} = $people->title(entry => $person->{'person'});
+		# Lookup the full name of the person
+		foreach my $person(@military) {
+			$person->{'title'} = $people->title(entry => $person->{'person'});
+		}
+
+		@military = sort { $a->{'title'} cmp $b->{'title'} } @military;
+
+		return $self->SUPER::html({ military => \@military, updated => $people->updated() });
 	}
-
-	@military = sort { $a->{'title'} cmp $b->{'title'} } @military;
-
-	return $self->SUPER::html({ military => \@military, updated => $people->updated() });
+	# No military database
+	return $self->SUPER::html();
 }
 
 1;
