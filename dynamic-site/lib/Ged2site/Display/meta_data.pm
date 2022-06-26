0d17d9515a9b114ebbf45941c7e3c51975eb1bd3
Nigel Horne <njh@bandsman.co.uk>
2022-06-25 22:37:02 -0400

    New VWF

--- dynamic-site/lib/Ged2site/Display/meta_data.pm
+++ dynamic-site/lib/Ged2site/Display/meta_data.pm
@@ -1,9 +1,9 @@
-package BBPortal::Display::meta_data;
+package Ged2site::Display::meta_data;
 
 use strict;
 use warnings;
 
-# Display the meta-data page
+# Display the meta-data page - the internal status of the server and VWF system
 
 use Ged2site::Display;
 
