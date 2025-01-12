6dbfca6faeaad4219403ee10f2ad44f8251351a2
Nigel Horne <njh@bandsman.co.uk>
2025-01-12 11:18:06 -0500

    Add the framework to display the history of an individual

--- dynamic-site/lib/Ged2site/Display/history.pm
+++ dynamic-site/lib/Ged2site/Display/history.pm
@@ -11,13 +11,31 @@ sub html {
 	my $self = shift;
 	my %args = (ref($_[0]) eq 'HASH') ? %{$_[0]} : @_;
 
-	my $eventshash;	# hash of year to array of events in that year, each event is a hash of the event's details
+	my $info = $self->{_info};
+	my $allowed = {
+		page => 'history',
+		entry => undef,	# TODO: Add regex for valid formats
+		lang => qr/^[A-Z][A-Z]/i,
+		lint_content => qr/^\d$/,
+		fbclid => qr/^[\w-]+$/i,	# Facebook tracking info
+		gclid => qr/^\w+$/i,	# Google tracking info
+	};
+	my %params = %{$info->params({ allow => $allowed })};
 
 	my $history = $args{'history'};	# Handle into the database
 
 	# TODO: handle situation where look up fails
-
-	my @events = $history->selectall_hash();
+        my @events;
+        if(my $entry = $params{'entry'}) {
+		# Display the timeline of one person
+		# TODO: add items such as birth of children, emigration, death of parents
+                @events = $history->selectall_hash({ person => $entry });
+        } else {
+                # Everyone
+                @events = $history->selectall_hash();
+        }
+	
+	my $eventshash;	# hash of year to array of events in that year, each event is a hash of the event's details
 	foreach my $event(@events) {
 		push @{$eventshash->{$event->{'year'}}}, $event;
 	}
