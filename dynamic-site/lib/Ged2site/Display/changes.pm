e39401408d0fb0bd63a9d30c795fde00d300ef16
Nigel Horne <njh@bandsman.co.uk>
2025-02-09 18:06:48 -0500

    Changes page - better sorting by date and type of change

--- dynamic-site/lib/Ged2site/Display/changes.pm
+++ dynamic-site/lib/Ged2site/Display/changes.pm
@@ -19,18 +19,36 @@ sub html {
 		'lint_content' => qr/^\d$/,
 	};
 	my %params = %{$info->params({ allow => $allowed })};
-	return "" if(delete($params{'page'}) ne 'changes');
+	return '' if(delete($params{'page'}) ne 'changes');
 
 	my $people = $args{'people'};
-	my @changes = $args{'changes'}->selectall_hash();
+	my @change_records = $args{'changes'}->selectall_hash();
 
-	foreach my $change(@changes) {
+	my $changes;
+
+	# Pass a hash ref to the template,
+	# key = the date of the change
+	# value = an array of hashes of
+	#	key = the type the change (e.g. new person, new date added)
+	#	value = an array of hashes of the record in changes.psv and the person record in people.csv
+	foreach my $record(@change_records) {
 		# Retrieve the person entry for this entry in the change table
 		#	and make it available to the template
 		# FIXME: this can take some time
-		$change->{'person'} = $people->fetchrow_hashref(entry => $change->{'xref'});
+		my $change_key = $record->{'change'};
+		if($change_key =~ /^Added date of birth/) {
+			$change_key = 'Added Birth Date';
+		} elsif($change_key =~ /^Added date of marriage/) {
+			$change_key = 'Added Marriage Date';
+		} elsif($change_key =~ /^New person/) {
+			$change_key = 'New People';
+		}
+		push @{$changes->{$record->{'date'}}->{$change_key}}, {
+			'record' => $record,
+			'person' => $people->fetchrow_hashref(entry => $record->{'xref'})
+		};
 	}
-	return $self->SUPER::html({ changes => \@changes, updated => $args{'changes'}->updated() });
+	return $self->SUPER::html({ changes => $changes, updated => $args{'changes'}->updated() });
 }
 
 1;
