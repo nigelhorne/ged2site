6c183de1dca2be98f4f6ba834102f7e0af24bd6a
Nigel Horne <njh@bandsman.co.uk>
2025-02-22 14:20:08 -0500

    Remove comma before closing bracket

--- dynamic-site/lib/Ged2site/Display/meta_data.pm
+++ dynamic-site/lib/Ged2site/Display/meta_data.pm
@@ -3,7 +3,7 @@ package Ged2site::Display::meta_data;
 use strict;
 use warnings;
 
-# Display the meta-data page
+# Display the meta-data page - the internal status of the server and VWF system
 
 use Ged2site::Display;
 
@@ -15,10 +15,11 @@ sub html
         my %args = (ref($_[0]) eq 'HASH') ? %{$_[0]} : @_;
 
 	my $vwf_log = $args{'vwf_log'};
+	my $domain_name = $self->{'info'}->domain_name();
 
 	my $datapoints;
 	foreach my $type('web', 'mobile', 'search', 'robot') {
-		my @entries = $vwf_log->type({ type => $type });
+		my @entries = $vwf_log->type({ domain_name => $domain_name, type => $type });
 		$datapoints .= '{y: ' . scalar(@entries) . ", label: \"$type\"},\n";
 		if($self->{'logger'}) {
 			$self->{'logger'}->debug("$type = " . scalar(@entries));
