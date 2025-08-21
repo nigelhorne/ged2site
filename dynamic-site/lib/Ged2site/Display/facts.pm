1c50f678b9f7c915e63ecd08493cfc645f8778f8
Nigel Horne <njh@bandsman.co.uk>
2025-08-20 21:43:44 -0400

    Remove duplicated code and improve facts page display

--- dynamic-site/lib/Ged2site/Display/facts.pm
+++ dynamic-site/lib/Ged2site/Display/facts.pm
@@ -10,16 +10,10 @@ use JSON::MaybeXS;
 
 our @ISA = ('Ged2site::Display');
 
-sub html {
+sub html
+{
 	my $self = shift;
 	my %args = (ref($_[0]) eq 'HASH') ? %{$_[0]} : @_;
-
-	my $allowed = {
-		'page' => 'facts',
-		'lang' => qr/^[A-Z][A-Z]/i,
-		'lint_content' => qr/^\d$/,
-	};
-	my %params = %{$self->{'_info'}->params({ allow => $allowed })};
 
 	my $json_file = File::Spec->catfile($args{'database_dir'}, 'facts.json');
 
@@ -27,28 +21,34 @@ sub html {
 	my $p;
 
 	if(open(my $json, '<', $json_file)) {
-		my $facts = JSON::MaybeXS->new()->utf8()->decode(<$json>);
+		my $facts;
+		eval {
+			local $/;	# Ensure all lines are slurped
+			$facts = JSON::MaybeXS->new()->decode(<$json>);
+		};
+		if ($@) {
+			$p->{'error'} = "Failed to parse $json_file: $@";
+			return $self->SUPER::html($p);
+		}
 		close($json);
-		if(my $fb = $facts->{'first_birth'}) {
-			$fb->{'person'} = $people->fetchrow_hashref(entry => delete $fb->{'xref'});
+
+		for my $key (qw(first_birth oldest_age most_children youngest_marriage oldest_marriage)) {
+			if(my $f = $facts->{$key}) {
+				$f->{'person'} = $people->fetchrow_hashref(entry => delete $f->{'xref'});
+			}
 		}
-		if(my $oa = $facts->{'oldest_age'}) {
-			$oa->{'person'} = $people->fetchrow_hashref(entry => delete $oa->{'xref'});
-		}
-		if(my $mc = $facts->{'most_children'}) {
-			$mc->{'person'} = $people->fetchrow_hashref(entry => delete $mc->{'xref'});
-		}
+		
 		if(my $bs = $facts->{'both_sides'}) {
 			foreach my $xref(@{$bs->{'xrefs'}}) {
 				push @{$bs->{'people'}}, $people->fetchrow_hashref(entry => $xref);
 			}
 			delete $bs->{'xrefs'};
 		}
-		if(my $ym = $facts->{'youngest_marriage'}) {
-			$ym->{'person'} = $people->fetchrow_hashref(entry => delete $ym->{'xref'});
-		}
-		if(my $om = $facts->{'oldest_marriage'}) {
-			$om->{'person'} = $people->fetchrow_hashref(entry => delete $om->{'xref'});
+		if(my $over_100s = $facts->{'people_over_100'}) {
+			foreach my $xref(@{$over_100s->{'xrefs'}}) {
+				push @{$over_100s->{'people'}}, $people->fetchrow_hashref(entry => $xref);
+			}
+			delete $over_100s->{'xrefs'};
 		}
 		if(my $lm = $facts->{'longest_marriage'}) {
 			$lm->{'person'} = $people->fetchrow_hashref(entry => delete $lm->{'xref'});
