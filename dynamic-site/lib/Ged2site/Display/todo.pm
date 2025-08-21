38ee606d4f98dca493ea75d55dc5e4d705636af9
Nigel Horne <njh@bandsman.co.uk>
2025-08-21 16:39:10 -0400

    Avoid blank summaries in the TODO file

--- dynamic-site/lib/Ged2site/Display/todo.pm
+++ dynamic-site/lib/Ged2site/Display/todo.pm
@@ -13,21 +13,21 @@ sub html {
 	my $self = shift;
 	my %args = (ref($_[0]) eq 'HASH') ? %{$_[0]} : @_;
 
-	my $todohash;	# hash of person's name to array of todos for that person, each todo is a hash of the todo's details
-
 	my $todo = $args{'todo'};	# Handle into the database
 	my $people = $args{'people'};
 
 	# TODO: handle situation where look up fails
 
 	# Create a list of entries in the TODO table, sorted by title
-	my @todos = sort { $a->{'title'} cmp $b->{'title'} } $todo->selectall_hash();
+	my @todos = sort { $a->{'title'} cmp $b->{'title'} } @{$todo->selectall_hashref()};
 
 	# Now create a list of hashes, each list is a list of entries in the todo table with the same summary field, the earlier
 	# sort ensures that the list will be sorted by title
+	my $todohash;	# hash of person's name to array of todos for that person, each todo is a hash of the todo's details
+
 	foreach my $t(@todos) {
 		# Ensure only list a person once per summary
-		push(@{$todohash->{$t->{'summary'}}}, $t) if(!grep { $_->{'title'} eq $t->{'title'} } @{$todohash->{$t->{'summary'}}});
+		push(@{$todohash->{$t->{'summary'}}}, $t) if(!grep { $_->{'title'} cmp $t->{'title'} } @{$todohash->{$t->{'summary'}}});
 	}
 
 	# use Data::Dumper;
