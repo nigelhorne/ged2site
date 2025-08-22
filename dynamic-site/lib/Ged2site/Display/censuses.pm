7627156c6cab43652734d63f64b9d73f912fa2e2
Nigel Horne <njh@bandsman.co.uk>
2025-08-22 07:28:38 -0400

    Check the validity of the censuses file

--- dynamic-site/lib/Ged2site/Display/censuses.pm
+++ dynamic-site/lib/Ged2site/Display/censuses.pm
@@ -6,16 +6,21 @@ use Ged2site::Display;
 
 use parent 'Ged2site::Display';
 
+our $allowed = {
+	'page' => 'censuses',
+	'census' => undef,	# TODO: regex of allowable name formats
+	'lang' => qr/^[A-Z]{2}$/i,
+	'lint_content' => qr/^\d$/
+};
+
 sub html {
 	my $self = shift;
 	my %args = (ref($_[0]) eq 'HASH') ? %{$_[0]} : @_;
 
-	my $allowed = {
-		'page' => 'censuses',
-		'census' => undef,	# TODO: regex of allowable name formats
-		'lang' => qr/^[A-Z]{2}$/i,
-		'lint_content' => qr/^\d$/,
-	};
+	if($self->{'logger'}) {
+		$self->{'logger'}->trace('> ' . __PACKAGE__ . '::html()');
+	}
+
 	# Handles into the databases
 	if(my $censuses = $args{'censuses'}) {
 		my $people = $args{'people'};
@@ -26,6 +31,10 @@ sub html {
 			# Display list of censuses
 			my @c = $censuses->census(distinct => 1);
 			@c = sort @c;
+			if($self->{'logger'}) {
+				$self->{'logger'}->debug('Display list of censuses');
+				$self->{'logger'}->trace('< ' . __PACKAGE__ . '::html()');
+			}
 			return $self->SUPER::html({ censuses => \@c, updated => $censuses->updated() });
 		}
 
@@ -36,16 +45,33 @@ sub html {
 		# Look in the censuses.csv for the name given as the CGI argument and
 		# find their details
 		if(my $census = $censuses->selectall_hashref({ census => $params->{'census'}})) {
-			my @people = sort { $a->{'title'} cmp $b->{'title'} } map { $people->fetchrow_hashref({ entry => $_->{'person'} }) } @{$census};
+			foreach my $census_entry(@{$census}) {
+				if(!defined($census_entry->{'xref'})) {
+					if($self->{'logger'}) {
+						$self->{'logger'}->warn(__PACKAGE__, ": census entry $census_entry->{entry} has no xref");
+					}
+				}
+			}
+			my @people = sort { $a->{'title'} cmp $b->{'title'} } map { $people->fetchrow_hashref({ entry => $_->{'xref'} }) } @{$census};
+			if($self->{'logger'}) {
+				$self->{'logger'}->debug("Display contents of census $params->{census}");
+				$self->{'logger'}->trace('< ' . __PACKAGE__ . '::html()');
+			}
 
 			return $self->SUPER::html({ census => $census, people => \@people, updated => $censuses->updated() });
-		} elsif($self->{'logger'}) {
+		}
+		if($self->{'logger'}) {
 			$self->{'logger'}->notice(__PACKAGE__, ": census $params->{census} not found");
+			$self->{'logger'}->trace('< ' . __PACKAGE__ . '::html()');
 		}
 		return $self->SUPER::html({ censuses => $censuses, error => "Census $params->{census} not found", updated => $censuses->updated() });
 	}
 	# No census database
-	return $self->SUPER::html();
+	if($self->{'logger'}) {
+		$self->{'logger'}->debug('No census database');
+		$self->{'logger'}->trace('< ' . __PACKAGE__ . '::html()');
+	}
+	return $self->SUPER::html({ error => 'No census database' });
 }
 
 1;
