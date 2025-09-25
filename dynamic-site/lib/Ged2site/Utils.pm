367d69d451088f260ef871e45510aafb1f023fd4
Nigel Horne <njh@bandsman.co.uk>
2025-09-25 15:25:06 -0400

    VWF fix: Don't set datastore when global is set to 0

--- dynamic-site/lib/Ged2site/Utils.pm
+++ dynamic-site/lib/Ged2site/Utils.pm
@@ -323,9 +323,11 @@ sub _configure_shared_memory($chi_args, $config, $args) {
 	$chi_args->{max_size} = $args->{'max_size'} || $config->{max_size} || 1024;
 }
 
-sub _configure_memory($chi_args, $config) {
-	$chi_args->{'global'} = $config->{'global'} // 1;
-	if(!$chi_args->{'global'}) {
+sub _configure_memory($chi_args, $config)
+{
+	if(exists $config->{'global'}) {
+		$chi_args->{'global'} = $config->{'global'};
+	} else {
 		$chi_args->{'datastore'} = {};
 	}
 }
